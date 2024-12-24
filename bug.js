2a2ec4a71b81e95de677b7dadfc7efe1977fff16
BugHunter-X <askzenai@gmail.com>
2024-12-24 07:14:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const requestListener = (request, response) => {
+  response.setHeader('Content-Type', 'application/json');
+  response.writeHead(200);
+  response.end(JSON.stringify({ message: 'Hello from Node.js!' }));
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(8080, () => {
+  console.log('Server is running on port 8080');
+});
+
+//Unhandled promise rejection error. This can happen when a promise is rejected and the error is not caught by a .catch() handler.
